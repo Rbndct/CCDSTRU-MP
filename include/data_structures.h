370e5c25e82502665b1433a3dd3e22e7b8e73a58
Rbee <rbenedict.maagma@gmail.com>
2025-04-02 03:27:12 +0800

    Remove unnecessary comments and include statement in data_structures.h

--- include/data_structures.h
+++ include/data_structures.h
@@ -1,12 +1,5 @@
 #ifndef DATA_STRUCTURES_H
 #define DATA_STRUCTURES_H
-/*
-Purpose:
-Defines structs for Position and MoveSet.
-Declares global variables for Uno, Dos, Tres, F, turn, go, over.
-*/
-
-#include <stdbool.h>
 
 // Define grid size based on A = {x ∈ ℤ+ | x < 5} (i.e., 4x4)
 #define GRID_SIZE 4
